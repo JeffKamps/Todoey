c3c2fd6ebc5cbfab566cb26c5429fdbb2e7ada8e
JeffKamps <jeffkamps@jeffkampss-mac-mini.local>
2018-02-24 16:35:25 -0800

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,29 +11,25 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
  
-    let defaults = UserDefaults.standard
+   // let defaults = UserDefaults.standard
       
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
+       
         
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggs"
-        itemArray.append(newItem2)
+        print(String(describing: dataFilePath))
         
-        let newItem3 = Item()
-        newItem3.title = "Get Lost"
-        itemArray.append(newItem3)
+        loadItems()
+        
+
         
         
-        if let item = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = item
-        }
+//        if let item = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = item
+//        }
        
     }
     // MARK - Tableview Datasource Methods
@@ -61,7 +57,8 @@ class TodoListViewController: UITableViewController {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 
-        tableView.reloadData()
+        saveItems()
+ 
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -80,8 +77,12 @@ class TodoListViewController: UITableViewController {
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            self.saveItems()
+            
+
+            
+          //  self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
@@ -90,6 +91,33 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion:  nil)
+    }
+    
+    // MARK = Model Munipulation Method
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item\(error)")
+            }
+        }
+        
     }
     
 }
